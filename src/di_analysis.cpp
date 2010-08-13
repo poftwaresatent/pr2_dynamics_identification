9831d9795d51af6e9d2702553a52bb8dffe68105
Roland Philippsen <roland.philippsen@gmx.net>
2010-08-13 13:41:47 -0700

    Trying to make the analysis into a two-step more advanced process, but not quite done with the filtering step. The idea is to segment the data stream into segments of constant-sign applied torque, divide each segment into two phases based on the velocity signal, and then estimate the static and viscous friction in order to compensate for them when we estimate the inertia.

--- src/di_analysis.cpp
+++ src/di_analysis.cpp
@@ -26,78 +26,343 @@
 #include <ros/ros.h>
 #include <dynamics_identification/Start.h>
 #include <dynamics_identification/Data.h>
-#include <dynamics_identification/Analysis.h>
+#include <Eigen/LU>
 #include <err.h>
-
-#undef USE_SVD
-#ifndef USE_SVD
-# include <Eigen/LU>
-#else // USE_SVD
-# include <Eigen/SVD>
-#endif // USE_SVD
 
 using namespace dynamics_identification;
 using namespace boost;
+using namespace std;
+
+
+struct setup_s {
+  setup_s(Start const & start)
+    : joint_name(start.joint_name),
+      lower_position(start.lower_position),
+      upper_position(start.upper_position),
+      torque_magnitude(start.torque_magnitude)
+  {
+  }
+  
+  inline bool operator != (Start const & start) const
+  {
+    return (joint_name != start.joint_name)
+      || (lower_position != start.lower_position)
+      || (upper_position != start.upper_position)
+      || (torque_magnitude != start.torque_magnitude);
+  }
+  
+  string joint_name;
+  float lower_position;
+  float upper_position;
+  float torque_magnitude;
+};
+
+
+struct segment_data_s {
+  segment_data_s(size_t setup_id_, size_t segment_id_, size_t smoothing_offset_)
+    : valid(false),
+      setup_id(setup_id_),
+      segment_id(segment_id_),
+      smoothing_offset(smoothing_offset_)
+  {
+  }
+  
+  bool valid;
+  size_t setup_id;
+  size_t segment_id;
+  
+  // from Data msg
+  vector<float> measured_milliseconds;
+  vector<float> measured_position;
+  vector<float> applied_torque;
+  double average_applied_torque;
+  
+  // scaled from ms and shifted origin s.t. seconds[0] == 0  
+  vector<double> seconds;
+  
+  // s.t. smoothed_position[ii+smoothing_offset] corresponds to measured_position[ii]
+  size_t smoothing_offset;
+  vector<double> smoothed_position;
+  vector<double> smoothed_velocity;
+  vector<double> smoothed_acceleration;
+  
+  // detected phase transitions (zero-crossings of smoothed_velocity)
+  // - phase1: decelerating towards zero velocity
+  // - phase2: accelerating towards upper/lower positional bound
+  //
+  // NOTE: For now we do not chop off data at the beginning and end of
+  // the phases, so these four indices could be encoded as just
+  // one. However, this way it's more explicit, and future compatible.
+  //
+  size_t phase1_begin;
+  size_t phase1_end;
+  size_t phase2_begin;
+  size_t phase2_end;
+};
+
+
+struct segment_analysis_s {
+  size_t setup_id;
+  size_t segment_id;
+  
+  // regression of x = A * exp(-t) + B * t + C
+  // stored as phaseX_a[0] == A  phaseX_a[1] == B  phaseX_a[2] == C
+  Eigen::Vector3d phase1_a;
+  Eigen::Vector3d phase2_a;
+  
+  // B_minus is the B parameter of the phase where velocity < 0 and
+  // B_plus where it is > 0
+  double B_minus;
+  double B_plus;
+  
+  // Estimated friction coefficients, assuming a simple static +
+  // viscous model of the form f_actual = f_applied + f_static +
+  // f_viscous where f_static = - sign(vel) * coeff_static and
+  // f_viscous = - coeff_viscous * vel
+  double friction_static;
+  double friction_viscous;
+  
+  // Estimated position, velocity, and acceleration, based on the
+  // exponential curve fit, and estimated actual torque and resulting
+  // inertia based on the friction model.
+  vector<double> phase1_position;
+  vector<double> phase1_velocity;
+  vector<double> phase1_acceleration;
+  vector<double> phase1_actual_torque;
+  vector<double> phase1_inertia;
+  vector<double> phase2_position;
+  vector<double> phase2_velocity;
+  vector<double> phase2_acceleration;
+  vector<double> phase2_actual_torque;
+  vector<double> phase2_inertia;
+};
+
+
+typedef shared_ptr<setup_s> setup_p;
+typedef shared_ptr<segment_data_s> segment_data_p;
+
+
+class Segmentation {
+public:
+  explicit Segmentation(size_t smoothing_range);
+  
+  /**
+     Append some data to the internal buffer, smoothing and segmenting
+     as we go along. Any segments that get completed via the received
+     data will be returned.
+     
+     \note Use getSetup() with the returned setup_id of the returned
+     segment to retrieve the full setup description. Also note that
+     the di_controller never sends data that mixes setups, so the
+     processData method is a fairly straightforward accumulation
+     function (except for the smoothing).
+     
+     \return A vector of newly completed segments. Can be empty, and
+     should usually be fairly small (maybe maximum two elements in
+     typical usage scenarios), so returning a vector by value should
+     be okay performance-wise.
+  */
+  vector<segment_data_p> processData(Data const & data);
+  
+  setup_p getSetup(size_t id) const;
+  
+protected:
+  static bool processSegment(segment_data_s & segment);
+  
+  size_t const smoothing_range_;
+  vector<setup_p> setup_;
+  segment_data_p buffer_;
+};
+
 
 static ros::Subscriber data_sub_;
-static ros::Publisher analysis_pub_;
-static size_t smoothing_range_(3);
+//static ros::Publisher analysis_pub_;
+static shared_ptr<Segmentation> segmentation_;
+
 
 static void data_cb(shared_ptr<Data const> const & data);
+
 
 int main(int argc, char*argv[])
 {
   ros::init(argc, argv, "di_analysis");
   ros::NodeHandle nn("~");
   
+  size_t smoothing_range(30);
   if (argc > 1) {
     int arg;
     if ((1 != sscanf(argv[1], "%i", &arg)) || (0 >= arg)) {
       errx(EXIT_FAILURE, "error reading argument `%s': positive number expected", argv[1]);
     }
-    smoothing_range_ = arg;
+    smoothing_range = arg;
   }
   
+  segmentation_.reset(new Segmentation(smoothing_range));
   data_sub_ = nn.subscribe<Data>("/di_controller/data", 1, data_cb);
-  analysis_pub_ = nn.advertise<Analysis>("analysis", 1);
+  //  analysis_pub_ = nn.advertise<Analysis>("analysis", 1);
   
   ros::spin();
 }
-  
+
+
 void data_cb(shared_ptr<Data const> const & data)
 {
-  size_t const twice_range(2 * smoothing_range_);
-  size_t const regr_npoints(twice_range + 1);
-  if (data->parameters.chunksize < regr_npoints) {
-    ROS_ERROR ("insufficient data points for regression (%zu points, need at least %zu)",
-	       data->parameters.chunksize, regr_npoints);
-    return;
+  vector<segment_data_p> segments(segmentation_->processData(*data));
+  for (vector<segment_data_p>::const_iterator is(segments.begin());
+       is != segments.end(); ++is) {
+    // analyse the segment...
+  }
+}
+
+
+Segmentation::
+Segmentation(size_t smoothing_range)
+  : smoothing_range_(smoothing_range)
+{
+}
+
+
+vector<segment_data_p> Segmentation::
+processData(Data const & data)
+{
+  vector<segment_data_p> completed_segments;
+  
+  //////////////////////////////////////////////////
+  // reject faulty data
+  
+  if (data.applied_torque.empty()) {
+    ROS_ERROR ("processData(): faulty data: empty torque data");
+    return completed_segments;
+  }
+  if ((data.applied_torque.size() != data.milliseconds.size())
+      || (data.applied_torque.size() != data.position.size())) {
+    ROS_ERROR ("processData(): faulty data: mismatching sizes");
+    return completed_segments;
   }
   
-  size_t const analysis_npoints(data->parameters.chunksize - twice_range);
+  //////////////////////////////////////////////////
+  // detect initialization and setup change
   
-  Analysis analysis_msg;
-  analysis_msg.measurement_parameters = data->parameters;
-  analysis_msg.smoothing_range = smoothing_range_;
-  analysis_msg.milliseconds.resize(analysis_npoints);
-  analysis_msg.measured_position.resize(analysis_npoints);
-  analysis_msg.measured_velocity.resize(analysis_npoints);
-  analysis_msg.applied_torque.resize(analysis_npoints);
-  analysis_msg.regression_0.resize(analysis_npoints);
-  analysis_msg.regression_1.resize(analysis_npoints);
-  analysis_msg.regression_2.resize(analysis_npoints);
-  analysis_msg.estimated_position.resize(analysis_npoints);
-  analysis_msg.estimated_velocity.resize(analysis_npoints);
-  analysis_msg.estimated_acceleration.resize(analysis_npoints);
-  analysis_msg.inertia.resize(analysis_npoints);
+  if (setup_.empty()) {
+    ROS_INFO ("processData(): initializing buffer");
+    buffer_.reset(new segment_data_s(0, 0, smoothing_range_));
+    setup_.push_back(setup_p(new setup_s(data.parameters)));
+  }
+  else if (*setup_.back() != data.parameters) {
+    ROS_INFO ("processData(): detected setup change");
+    completed_segments.push_back(buffer_);
+    buffer_.reset(new segment_data_s(setup_.size(), 0, smoothing_range_));
+    setup_.push_back(setup_p(new setup_s(data.parameters)));
+  }
   
-  for (size_t i_out(0); i_out < analysis_npoints; ++i_out) {
+  //////////////////////////////////////////////////
+  // detect sign change in applied torque
+  
+  if (( ! buffer_->applied_torque.empty())
+      && (((buffer_->applied_torque.back() > 0) && (data.applied_torque.front() < 0))
+	  || ((buffer_->applied_torque.back() < 0) && (data.applied_torque.front() > 0)))) {
+    ROS_INFO ("processData(): sign change in applied torque across data chunks");
+    completed_segments.push_back(buffer_);
+    buffer_.reset(new segment_data_s(buffer_->setup_id,
+				     buffer_->segment_id + 1,
+				     smoothing_range_));
+  }
+  
+  size_t const datasize(data.applied_torque.size());
+  for (size_t at_start(0); at_start < datasize; /**/) {
     
-    size_t i_in(i_out + smoothing_range_);
-    analysis_msg.milliseconds[i_out] = data->milliseconds[i_in];
-    analysis_msg.measured_position[i_out] = data->position[i_in];
-    analysis_msg.measured_velocity[i_out] = data->velocity[i_in];
-    analysis_msg.applied_torque[i_out] = data->applied_torque[i_in];
+    float const at0(data.applied_torque[at_start]);
+    size_t at_change(at_start + 1);
+    if (at0 > 0) {
+      for (/**/; at_change < datasize; ++at_change) {
+	if (data.applied_torque[at_change] < 0) {
+	  break;
+	}
+      }
+    }
+    else {		    // XXXX potential for bug if at0 == 0
+      for (/**/; at_change < datasize; ++at_change) {
+	if (data.applied_torque[at_change] > 0) {
+	  break;
+	}
+      }
+    }
+    
+    size_t const segment_length(at_change - at_start);
+    ROS_INFO ("processData(): buffering segment of length %zu", segment_length);
+    
+    buffer_->measured_milliseconds.resize(segment_length);
+    buffer_->measured_position.resize(segment_length);
+    buffer_->applied_torque.resize(segment_length);
+    size_t const nbytes(segment_length * sizeof(float));
+    memcpy(&buffer_->measured_milliseconds[0], &data.milliseconds[0], nbytes);
+    memcpy(&buffer_->measured_position[0], &data.position[0], nbytes);
+    memcpy(&buffer_->applied_torque[0], &data.applied_torque[0], nbytes);
+    
+    if (at_change < datasize) {
+      // sign change within
+      ROS_INFO ("processData(): completed segment from %zu to %zu", at_start, at_change);
+      completed_segments.push_back(buffer_);
+      buffer_.reset(new segment_data_s(buffer_->setup_id,
+				       buffer_->segment_id + 1,
+				       smoothing_range_));
+    }
+    
+    at_start = at_change;
+  }
+  
+  //////////////////////////////////////////////////
+  // We have extracted the segments based on sign changes of the
+  // applied torque, now we just have to smooth out their velocity
+  // data to detect their phases.
+  
+  for (vector<segment_data_p>::const_iterator is(completed_segments.begin());
+       is != completed_segments.end(); ++is) {
+    (*is)->valid = processSegment(**is);
+  }
+  
+  return completed_segments;
+}
+
+
+bool Segmentation::
+processSegment(segment_data_s & segment)
+{
+  ROS_INFO ("processSegment(): setup: %zu  segment: %zu", segment.setup_id, segment.segment_id);
+  
+  size_t const twice_range(2 * segment.smoothing_offset);
+  size_t const regr_npoints(twice_range + 1);
+  size_t const datasize(segment.measured_milliseconds.size());
+  if (datasize < regr_npoints) {
+    ROS_ERROR ("processSegment(): %zu data points, need at least %zu for regression",
+	       datasize, regr_npoints);
+    return false;
+  }
+  
+  //////////////////////////////////////////////////
+  // compute average applied torque and shifted timescale
+  
+  segment.average_applied_torque = 0;
+  segment.seconds.resize(datasize);
+  float const ms0(segment.measured_milliseconds[0]);
+  for (size_t ii(0); ii < datasize; ++ii) {
+    segment.average_applied_torque += segment.applied_torque[ii];
+    segment.seconds[ii] = 1e-3 * (segment.measured_milliseconds[ii] - ms0);
+  }
+  segment.average_applied_torque /= datasize;
+  ROS_INFO ("processSegment(): average applied torque %g", segment.average_applied_torque);
+  
+  //////////////////////////////////////////////////
+  // compute smoothed trajectory for subsequent phase detection
+  
+  size_t const smoothed_npoints(datasize - twice_range);
+  ROS_INFO ("processSegment(): smoothing %zu points", smoothed_npoints);
+  
+  segment.smoothed_position.resize(smoothed_npoints);
+  segment.smoothed_velocity.resize(smoothed_npoints);
+  segment.smoothed_acceleration.resize(smoothed_npoints);
+  
+  for (size_t i_out(0); i_out < smoothed_npoints; ++i_out) {
     
     // Polynomial regression of q(t) = a0 + a1*t + a2*t^2
     // - yy = vector of measured positions
@@ -105,47 +370,89 @@ void data_cb(shared_ptr<Data const> const & data)
     // - aa = vector of polynomial parameters a0, a1, ...
     Eigen::VectorXd yy(regr_npoints);
     Eigen::MatrixXd XX(regr_npoints, 3);
-    i_in = i_out;
-    double const t0(analysis_msg.milliseconds[i_out] * 1e-3);
+    size_t i_in(i_out);
+    // The regression time origin is centered on smoothing window for
+    // better results and for more straightforward equations for the
+    // smoothed pos, vel, and acc further down.
+    double const t0(segment.seconds[i_in + segment.smoothing_offset]);
     for (size_t i_regr(0); i_regr < regr_npoints; ++i_regr, ++i_in) {
-      yy[i_regr] = data->position[i_in];
-      double const ts((data->milliseconds[i_in] - analysis_msg.milliseconds[i_out]) * 1e-3);
+      yy[i_regr] = segment.measured_position[i_in];
+      double const ts(segment.seconds[i_in] - t0);
       XX.coeffRef(i_regr, 0) = 1;
-      XX.coeffRef(i_regr, 1) = ts;
+      XX.coeffRef(i_regr, 1) = ts; // yes yes, ts will be zero on center sample, so what?
       XX.coeffRef(i_regr, 2) = pow(ts, 2);
     }
-    
-#ifndef USE_SVD
-
     Eigen::MatrixXd toto(XX.transpose() * XX);
     Eigen::VectorXd const aa(toto.inverse() * XX.transpose() * yy);
     
-#else // USE_SVD
-
-    // Least-squares via SVD. Should be just a plain inversion though,
-    // weird that it did not always work.
-    Eigen::SVD<Eigen::MatrixXd> svd(XX.transpose() * XX);
-    Eigen::MatrixXd toto(Eigen::MatrixXd::Zero(3, 3));
-    Eigen::VectorXd const & sigma(svd.singularValues());
-    for (size_t jj(0); jj < 3; ++jj) {
-      if (sigma[jj] > 1e-3) {
-	toto(jj, jj) = 1 / sigma[jj];
-      }
-    }
-    Eigen::MatrixXd pseudoinv(svd.matrixV() * toto * svd.matrixU());
-    Eigen::VectorXd const aa(pseudoinv * XX.transpose() * yy);
-
-#endif // USE_SVD
-    
-    double const tnow(analysis_msg.milliseconds[i_out] * 1e-3);
-    analysis_msg.regression_0[i_out] = aa[0];
-    analysis_msg.regression_1[i_out] = aa[1];
-    analysis_msg.regression_2[i_out] = aa[2];
-    analysis_msg.estimated_position[i_out] = aa[0];
-    analysis_msg.estimated_velocity[i_out] = aa[1];
-    analysis_msg.estimated_acceleration[i_out] = 2 * aa[2];
-    analysis_msg.inertia[i_out] = analysis_msg.applied_torque[i_out] / 2 / aa[2];
+    segment.smoothed_position[i_out] = aa[0];
+    segment.smoothed_velocity[i_out] = aa[1];
+    segment.smoothed_acceleration[i_out] = 2 * aa[2];
   }
   
-  analysis_pub_.publish(analysis_msg);
+  //////////////////////////////////////////////////
+  // detect zero-crossing of smoothed_velocity, assuming we only have
+  // one such sign change (which is not guaranteed when segments do
+  // not come from clean experiments, so let's at least flag such
+  // segments as invalid).
+  
+  double const vel0(segment.smoothed_velocity[0]);
+  size_t vel_change(1);
+  if (vel0 > 0) {
+    for (/**/; vel_change < smoothed_npoints; ++vel_change) {
+      if (segment.smoothed_velocity[vel_change] < 0) {
+	ROS_INFO ("processSegment(): zero crossing to <0 at %zu +offset", vel_change);
+	break;
+      }
+    }
+  }
+  else {		    // XXXX potential for bug if vel0 == 0
+    for (/**/; vel_change < smoothed_npoints; ++vel_change) {
+      if (segment.smoothed_velocity[vel_change] > 0) {
+	ROS_INFO ("processSegment(): zero crossing to >0 at %zu +offset", vel_change);
+	break;
+      }
+    }
+  }
+  if (vel_change >= smoothed_npoints) {
+    ROS_ERROR ("smoothed velocity does not change sign, setup %zu, segment %zu",
+	       segment.setup_id, segment.segment_id);
+    return false;
+  }
+
+  size_t second_vel_change(vel_change + 1);
+  if (vel0 > 0) {
+    for (/**/; second_vel_change < smoothed_npoints; ++second_vel_change) {
+      if (segment.smoothed_velocity[second_vel_change] < 0) {
+	ROS_INFO ("processSegment(): 2nd zero crossing to <0 at %zu +offset", second_vel_change);
+	break;
+      }
+    }
+  }
+  else {		    // XXXX potential for bug if vel0 == 0
+    for (/**/; second_vel_change < smoothed_npoints; ++second_vel_change) {
+      if (segment.smoothed_velocity[second_vel_change] > 0) {
+	ROS_INFO ("processSegment(): 2nd zero crossing to >0 at %zu +offset", second_vel_change);
+	break;
+      }
+    }
+  }
+  if (second_vel_change < smoothed_npoints) {
+    ROS_ERROR ("smoothed velocity changes sign more than once, setup %zu, segment %zu",
+	       segment.setup_id, segment.segment_id);
+    return false;
+  }
+  
+  //////////////////////////////////////////////////
+  // Hooray we passed all tests!
+  
+  ROS_INFO ("processSegment(): segment is valid");
+  
+  segment.valid = true;
+  segment.phase1_begin = 0;
+  segment.phase1_end = vel_change + segment.smoothing_offset;
+  segment.phase2_begin = segment.phase1_end;
+  segment.phase2_end = datasize;
+  
+  return true;
 }
